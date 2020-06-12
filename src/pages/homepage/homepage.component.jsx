d7fa3a5a85e72d56a7151a1cf892eb0f9a63cf7a
adrian mustea <mustea.adrian95@gmail.com>
2020-06-12 15:16:50 +0300

    init

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory-component";
+import MenuItem from "../../components/menu-item/menu-item.component";
+const HomePage = (props) => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
