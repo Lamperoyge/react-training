56cea7ccd51cf1fa039b733a9bef7ca2116a2427
adrian mustea <mustea.adrian95@gmail.com>
2020-06-15 16:52:21 +0300

    added protected route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useEffect } from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInSignUp from "./pages/auth/sign-in-sign-up.component";
@@ -36,16 +36,27 @@ function App(props) {
       <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/signin" component={SignInSignUp} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            props.currentUser ? <Redirect to="/" /> : <SignInSignUp />
+          }
+        />
         <Route path="/shop" component={ShopPage} />
       </Switch>
     </Fragment>
   );
 }
 
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.user.currentUser,
+  };
+};
 const mapDispatchToProps = (dispatch) => {
   return {
     setCurrentUser: (user) => dispatch(setCurrentUser(user)),
   };
 };
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
